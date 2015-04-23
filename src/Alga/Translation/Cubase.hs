2468a37374ff0c37f774c09332379439c77da65f
mrkkrp <markkarpov@opmbx.org>
2015-04-23 13:48:31 +0600

    first attempt on Cubase backend

--- src/Alga/Translation/Cubase.hs
+++ src/Alga/Translation/Cubase.hs
@@ -17,11 +17,46 @@
 -- You should have received a copy of the GNU General Public License along
 -- with this program. If not, see <http://www.gnu.org/licenses/>.
 
+{-# LANGUAGE Arrows #-}
+
 module Alga.Translation.Cubase (cubaseBackend) where
 
+import Control.Arrow
+import qualified Data.Map.Lazy as M
+
+import Control.Arrow.ArrowTree
 import Text.XML.HXT.Core
 
 import Alga.Translation.Base
 
+infixr 5 />/
+
 cubaseBackend :: AutoMap -> IOSArrow XmlTree XmlTree
-cubaseBackend = undefined
+cubaseBackend m = "tracklist" />/ "list" />/ processChildren (procTrack m)
+
+procTrack :: (ArrowXml a, ArrowChoice a) => AutoMap -> a XmlTree XmlTree
+procTrack m = (trackName >>> arr (`M.lookup` m)) &&& this >>>
+              proc (x, tree) ->
+                  case x of
+                    Nothing -> this     -< tree
+                    Just b  -> procAuto -< (b, tree)
+
+procAuto :: ArrowXml a => a (AutoBatch, XmlTree) XmlTree
+procAuto = arr snd >>> replaceChildren (txt "Hello, Cubase!")
+
+trackName :: ArrowXml a => a XmlTree String
+trackName = alt ofClass cs /> ofClass "MListNode" /> getStr "Name"
+    where cs = ["MAudioTrackEvent","MInstrumentTrackEvent","MDeviceTrackEvent"]
+
+alt :: ArrowXml a => (b -> a XmlTree XmlTree) -> [b] -> a XmlTree XmlTree
+alt a = foldr ((<+>) . a) this
+
+ofClass :: ArrowXml a => String -> a XmlTree XmlTree
+ofClass cls = isElem >>> hasName "obj" >>> hasAttrValue "class" (== cls)
+
+getStr :: ArrowXml a => String -> a XmlTree String
+getStr name = isElem >>> hasName "string" >>>
+              hasAttrValue "name" (== name) >>> getAttrValue "value"
+
+(/>/) :: ArrowXml a => String -> a XmlTree XmlTree -> a XmlTree XmlTree
+name />/ action = processChildren $ action `when` (isElem >>> hasName name)
